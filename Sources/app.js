7e6d0797a4f96853e6fb36e0efa5ae7ab7a24f8a
serment7 <serment7@gmail.com>
2020-03-29 16:52:29 +0900

    no message

---
+++ Sources/app.js
@@ -0,0 +1,32 @@
+﻿
+
+
+webix.ready(function () {
+    webix.ui({
+        view: "form",
+        elements: [{
+            rows: [
+                {
+                    cols: [{},
+                        {
+                            view: "sidebar", css:"webix_dark", width: 200, 
+                            data: [
+                                {
+                                    id: "charp", value: "C#"
+                                },
+                                {
+                                    id: "cplusplus", value:"C++"
+                                },
+                                {
+                                    id: "algorithm", value: "Algorithm"
+                                },
+
+                                ]
+                        }
+                    ]
+                }
+            ]
+        }]
+    });
+
+})
